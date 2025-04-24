3d56142defbd1746d8645e06b397feaac51b7f51
Евгения Репникова <evrepni@gmail.com>
2025-04-25 02:18:08 +0500

    feat: implement list and read

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import { join } from "path";
+import { readdir } from "fs/promises";
+
+const __dirname = import.meta.dirname;
+
+const pathToFiles = join(__dirname, "files");
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await readdir(pathToFiles);
+    console.log(files);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { readFile } from "fs/promises";
+import { join } from "path";
+
+const __dirname = import.meta.dirname;
+
+const path = join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const data = await readFile(path, "utf-8");
+    console.log(data);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
