85c32695f80953beb0899c4e28e420e8399fdf9e
Евгения Репникова <evrepni@gmail.com>
2025-04-25 00:59:58 +0500

    feat: implement copy and create

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import { join } from "path";
+import { copyFile, mkdir, readdir } from "node:fs/promises";
+
+const __dirname = import.meta.dirname;
+
+const pathToFiles = join(__dirname, "files");
+const pathCopy = join(__dirname, "files_copy");
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    const files = await readdir(pathToFiles);
+    await mkdir(pathCopy);
+
+    await Promise.all(
+      files.map(async (file) => {
+        const fromPath = join(pathToFiles, file);
+        const toPath = join(pathCopy, file);
+        await copyFile(fromPath, toPath);
+      })
+    );
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
