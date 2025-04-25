9f2d435194631486878e7199ef959812703bfb8b
Евгения Репникова <evrepni@gmail.com>
2025-04-26 03:22:06 +0500

    feat: implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
+import { spawn } from "child_process";
+import { join } from "path";
+
+const dirname = import.meta.dirname;
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const filePath = join(dirname, "files", "script.js");
+
+  const childProcess = spawn("node", [filePath, ...args], {
+    stdio: ["pipe", "pipe", "pipe", "ipc"],
+  });
+
+  process.stdin.pipe(childProcess.stdin);
+
+  childProcess.stdout.pipe(process.stdout);
+
+  childProcess.on("error", (err) => {
+    throw err;
+  });
+  return childProcess;
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1", "arg2", "arg3"]);
