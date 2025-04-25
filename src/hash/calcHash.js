e954b3aa518911d03eab1b1c9585cd39d3b99e61
Евгения Репникова <evrepni@gmail.com>
2025-04-25 22:57:40 +0500

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import path from "path";
+import { createHash } from "node:crypto";
+import fs from "node:fs";
 
+const calculateHash = async () => {
+  const hash = createHash("sha256");
+
+  const dirname = import.meta.dirname;
+
+  const hashPath = path.join(dirname, "files", "fileToCalculateHashFor.txt");
+  const input = fs.createReadStream(hashPath);
+
+  input.on("readable", () => {
+    const data = input.read();
+    if (data) {
+      hash.update(data);
+    }
+  });
+
+  input.on("end", () => {
+    console.log(hash.digest("hex"));
+  });
+};
 await calculateHash();
